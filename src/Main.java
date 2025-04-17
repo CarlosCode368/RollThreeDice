8552c31338de023837f7022e62ca2b2bcbb24d01
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-17 11:35:13 -0700

    dice

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Die{
+    public static int roll(){
+        int minValue=1;
+        int maxValue=6;
+        int randomNumber=(int)(Math.random()*maxValue)+minValue;
+        return randomNumber;
+    }
+}
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static String bar(int qty) {
+        String output="";
+        for (int b = 0; b < qty; b++) {
+            output+="#";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        //3-18 are the possible results
+        int[] counters = new int[16]; //16 slots 18-3
+        //roll roll roll *500
+        for (int i = 0; i < 500; i++) {
+            int value = Die.roll() + Die.roll() + Die.roll();
+            counters[value - 3]++; //++ is increase one (+1)
+        }
+        //print qty for each slot
+        for (int s = 3; s <= 18; s++) {
+            int qty = counters[s - 3];
+            System.out.printf("|%s (%d:%d)\n", bar(qty),s,qty);
         }
     }
 }
+
+
+
+
+
+
